9f51c0a3b271f36c8ff4a659d44f50c07e02d90a
Armand <arramband@gmail.com>
2025-07-23 15:54:41 +0200

    Added docstrings to Blog model.

--- blog_app/models.py
+++ blog_app/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-
+    """
+    Represents a Blog post.
+    
+    Attributes:
+        author - Foreign key linked to user account
+        title - The title of the post
+        body - The content of the post
+    """
     author = models.ForeignKey('auth.user', 
                                 on_delete=models.CASCADE, 
                                 null=True, 
@@ -11,4 +18,5 @@ class Blog(models.Model):
     body = models.TextField()
 
     def __str__(self) -> str:
+        """Returns a string representation of the model."""
         return f"{self.author.username} - {self.title}"
